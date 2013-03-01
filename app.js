0a7cb74e2acfa0025e529f1ccd777dc13bbe2483
Bushi <bushi.zhang@lyriqal.com>
2013-02-28 20:44:41 -0500

    added zindex to icon

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.configure(function(){
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+}); 
 
 app.configure('production', function(){
   app.use(express.errorHandler());
